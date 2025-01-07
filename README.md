8d1f2358dff856cb9033053a5668ce06118b00b2
DacSanDakLakNinhThuan <hungmaiyeunhaudamsau@gmail.com>
2025-01-07 16:48:30 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-Translation Component
-=====================
+Routing Component
+=================
 
-The Translation component provides tools to internationalize your application.
+The Routing component maps an HTTP request to a set of configuration variables.
 
 Resources
 ---------
 
-  * [Documentation](https://symfony.com/doc/current/components/translation/index.html)
+  * [Documentation](https://symfony.com/doc/current/components/routing/index.html)
   * [Contributing](https://symfony.com/doc/current/contributing/index.html)
   * [Report issues](https://github.com/symfony/symfony/issues) and
     [send Pull Requests](https://github.com/symfony/symfony/pulls)
--- Tests/Loader/PhpFileLoaderTest.php
+++ Tests/Loader/PhpFileLoaderTest.php
@@ -9,41 +9,74 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Translation\Tests\Loader;
+namespace Symfony\Component\Routing\Tests\Loader;
 
-use Symfony\Component\Translation\Loader\PhpFileLoader;
-use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\PhpFileLoader;
 
 class PhpFileLoaderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testLoad()
+    public function testSupports()
     {
-        $loader = new PhpFileLoader();
-        $resource = __DIR__.'/../fixtures/resources.php';
-        $catalogue = $loader->load($resource, 'en', 'domain1');
+        $loader = new PhpFileLoader($this->getMock('Symfony\Component\Config\FileLocator'));
 
-        $this->assertEquals(array('foo' => 'bar'), $catalogue->all('domain1'));
-        $this->assertEquals('en', $catalogue->getLocale());
-        $this->assertEquals(array(new FileResource($resource)), $catalogue->getResources());
+        $this->assertTrue($loader->supports('foo.php'), '->supports() returns true if the resource is loadable');
+        $this->assertFalse($loader->supports('foo.foo'), '->supports() returns true if the resource is loadable');
+
+        $this->assertTrue($loader->supports('foo.php', 'php'), '->supports() checks the resource type if specified');
+        $this->assertFalse($loader->supports('foo.php', 'foo'), '->supports() checks the resource type if specified');
     }
 
-    /**
-     * @expectedException \Symfony\Component\Translation\Exception\NotFoundResourceException
-     */
-    public function testLoadNonExistingResource()
+    public function testLoadWithRoute()
     {
-        $loader = new PhpFileLoader();
-        $resource = __DIR__.'/../fixtures/non-existing.php';
-        $loader->load($resource, 'en', 'domain1');
+        $loader = new PhpFileLoader(new FileLocator(array(__DIR__.'/../Fixtures')));
+        $routeCollection = $loader->load('validpattern.php');
+        $routes = $routeCollection->all();
+
+        $this->assertCount(1, $routes, 'One route is loaded');
+        $this->assertContainsOnly('Symfony\Component\Routing\Route', $routes);
+
+        foreach ($routes as $route) {
+            $this->assertSame('/blog/{slug}', $route->getPath());
+            $this->assertSame('MyBlogBundle:Blog:show', $route->getDefault('_controller'));
+            $this->assertSame('{locale}.example.com', $route->getHost());
+            $this->assertSame('RouteCompiler', $route->getOption('compiler_class'));
+            $this->assertEquals(array('GET', 'POST', 'PUT', 'OPTIONS'), $route->getMethods());
+            $this->assertEquals(array('https'), $route->getSchemes());
+        }
     }
 
-    /**
-     * @expectedException \Symfony\Component\Translation\Exception\InvalidResourceException
-     */
-    public function testLoadThrowsAnExceptionIfFileNotLocal()
+    public function testLoadWithImport()
     {
-        $loader = new PhpFileLoader();
-        $resource = 'http://example.com/resources.php';
-        $loader->load($resource, 'en', 'domain1');
+        $loader = new PhpFileLoader(new FileLocator(array(__DIR__.'/../Fixtures')));
+        $routeCollection = $loader->load('validresource.php');
+        $routes = $routeCollection->all();
+
+        $this->assertCount(1, $routes, 'One route is loaded');
+        $this->assertContainsOnly('Symfony\Component\Routing\Route', $routes);
+
+        foreach ($routes as $route) {
+            $this->assertSame('/prefix/blog/{slug}', $route->getPath());
+            $this->assertSame('MyBlogBundle:Blog:show', $route->getDefault('_controller'));
+            $this->assertSame('{locale}.example.com', $route->getHost());
+            $this->assertSame('RouteCompiler', $route->getOption('compiler_class'));
+            $this->assertEquals(array('GET', 'POST', 'PUT', 'OPTIONS'), $route->getMethods());
+            $this->assertEquals(array('https'), $route->getSchemes());
+        }
+    }
+
+    public function testThatDefiningVariableInConfigFileHasNoSideEffects()
+    {
+        $locator = new FileLocator(array(__DIR__.'/../Fixtures'));
+        $loader = new PhpFileLoader($locator);
+        $routeCollection = $loader->load('with_define_path_variable.php');
+        $resources = $routeCollection->getResources();
+        $this->assertCount(1, $resources);
+        $this->assertContainsOnly('Symfony\Component\Config\Resource\ResourceInterface', $resources);
+        $fileResource = reset($resources);
+        $this->assertSame(
+            realpath($locator->locate('with_define_path_variable.php')),
+            (string) $fileResource
+        );
     }
 }
